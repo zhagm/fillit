33a469c32b73d30fc83524fff1957e6fbe809bc7
Zhaganbike Magauina <zmagauin@e1z2r9p45.42.us.org>
2019-02-01 14:23:00 -0800

    merged, small modifications, new libft function

--- check_valid_file.c
+++ check_valid_file.c
@@ -39,6 +39,56 @@ int		check_tet_format(char *tet)
 	return (1);
 }
 
+char	**trim_tet_array(char **tet_arr)
+{
+	return (tet_array);
+}
+
+t_tet	*make_new_tet(char **tet_array)
+{
+	int 	x;
+	int 	y;
+	int		*size_array;
+	t_tet	*new_tet;
+
+	size_array = (int *)malloc(2 * sizeof(int));
+	x = 0;
+	while (tet_array[x])
+		x++;
+	while (tet_array[0][y])
+		y++;
+	size_array[0] = x;
+	size_array[1] = y;
+	new_tet->size = size_array;
+	new_tet->tetrimino = trim_tet_array(tet_array);
+
+	return (new_tet);
+}
+
+void	print_array(char **array)
+{
+	// int i;
+	// int j;
+
+	// i = 0;
+	// while (array[i])
+	// {
+	// 	j = 0;
+	// 	while (array[i][j])
+	// 	{
+	// 		printf("%");
+	// 	}
+	// }
+	printf("ARRAY PRINT PLACEHOLDER (WRITE CODE HERE)");
+}
+
+void	print_tet(t_tet *tet)
+{
+	printf("Tetrimino array: \n");
+	print_array(tet->tetrimino);
+	printf("size: [%d, %d]\n", tet->size[0], tet->size[1]);
+}
+
 int		check_valid_tetrimino(char *tet, t_tet **tet_list)
 {
 	int		count;
@@ -51,8 +101,10 @@ int		check_valid_tetrimino(char *tet, t_tet **tet_list)
 	count = get_sides_count(tet_array); // --------------------------------------------Check validity of tets with get_sides_count
 	if (count == 6 || count == 8) // -------------------------------------------------------If all checks pass, create list item for tet and add to tet_list
 	{
-		// new_item = ft_tet_lstnew(tet_array);
+		new_item = make_new_tet(tet_array);
+		print_tet(new_item);
 		// ft_tet_lstadd(tet_list, new_item);
+		// free tet_array!!
 		return (1);
 	}
 	return (0);
